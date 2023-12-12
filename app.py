2bcd9da2b1840c749bb0df0bdb1c3fec1382495c
nikita <nikita03.wot@gmail.com>
2023-12-12 23:51:17 +0300

    v 2.0

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, jsonify
 import sqlite3
+import re
 
 app = Flask(__name__)
 
@@ -10,7 +11,13 @@ def create_employee():
         name = request.form['name']
         phone = request.form['phone']
 
-        conn = sqlite3.connect('internet_shop.db')
+        if len(name) > 12 or re.search(r'\d', name):
+            return "Введите корректное имя"
+
+        if not re.match(r'^\+\d{10,15}$', phone):
+            return "Введите номер через + без пробелом, попробуйте снова"
+
+        conn = sqlite3.connect('i_shop.db')
         cursor = conn.cursor()
 
         cursor.execute('INSERT INTO employees (name, phone) VALUES (?, ?)', (name, phone))
@@ -23,7 +30,7 @@ def create_employee():
 
 @app.route('/get_employees', methods=['GET'])
 def get_all_employees():
-    conn = sqlite3.connect('internet_shop.db')
+    conn = sqlite3.connect('i_shop.db')
     cursor = conn.cursor()
 
     cursor.execute('SELECT * FROM employees')
@@ -42,7 +49,13 @@ def update_employee():
         phone = request.form['phone']
 
         try:
-            conn = sqlite3.connect('internet_shop.db')
+            if not re.match(r'^[A-Za-z\s]+$', name):
+                return "Ошибка: Неверный формат имени, введите корректно"
+
+            if not re.match(r'^\+\d{10,15}$', phone):
+                return "Ошибка: Неверный формат номера телефона, введите корректно"
+
+            conn = sqlite3.connect('i_shop.db')
             cursor = conn.cursor()
 
             cursor.execute('UPDATE employees SET name = ?, phone = ? WHERE id = ?', (name, phone, employee_id))
@@ -61,8 +74,10 @@ def delete_employee():
     if request.method == 'POST':
         employee_id = request.form['employee_id']
 
+        if not employee_id.isdigit():
+            return "Ошибка: Неверный формат идентификатора сотрудника"
         try:
-            conn = sqlite3.connect('internet_shop.db')
+            conn = sqlite3.connect('i_shop.db')
             cursor = conn.cursor()
 
             cursor.execute('DELETE FROM employees WHERE id = ?', (employee_id,))
@@ -100,19 +115,32 @@ def manage_orders():
 def create_client():
     if request.method == 'POST':
         full_name = request.form['full_name']
+        surname = request.form['surname']
         email = request.form['email']
         phone = request.form['phone']
 
+        if not re.match(r'^[A-Za-zА-Яа-яЁё\s]+$', full_name):
+            return "Ошибка: Неверный формат имени"
+
+        if not re.match(r'^[A-Za-zА-Яа-яЁё\s]+$', surname):
+            return "Ошибка: Неверный форма фамилии "
+
+        if not re.match(r'^\+\d{10,15}$', phone):
+            return "Ошибка: Неверный формат номера телефона, введите корректно"
+
+        if not re.match(r'^[a-zA-Z0-9._%+-]+@gmail\.com$', email):
+            return "Ошибка: Неверный формат адреса электронной почты, введите корректно"
+
         try:
-            conn = sqlite3.connect('internet_shop.db')
+            conn = sqlite3.connect('i_shop.db')
             cursor = conn.cursor()
 
-            cursor.execute('INSERT INTO clients (full_name, email, phone) VALUES (?, ?, ?)', (full_name, email, phone))
+            cursor.execute('INSERT INTO clients (full_name, surname, email, phone) VALUES (?, ?, ?, ?)', (full_name, surname, email, phone))
 
             conn.commit()
             conn.close()
 
-            return redirect(url_for('index'))  # Предполагается, что 'index' - это ваша главная страница
+            return redirect(url_for('index'))
         except Exception as e:
             print("Ошибка при создании клиента:", e)
             return "Ошибка при создании клиента"
@@ -121,7 +149,7 @@ def create_client():
 @app.route('/get_all_clients', methods=['GET'])
 def get_all_clients():
     try:
-        conn = sqlite3.connect('internet_shop.db')
+        conn = sqlite3.connect('i_shop.db')
         cursor = conn.cursor()
 
         cursor.execute('SELECT * FROM clients')
@@ -134,23 +162,144 @@ def get_all_clients():
         print("Ошибка при получении списка клиентов:", e)
         return "Ошибка при получении списка клиентов"
 
+@app.route('/delete_client', methods=['POST'])
+def delete_client():
+    if request.method == 'POST':
+        client_id = request.form['client_id']
 
-@app.route('/get_all_orders', methods=['GET'])
-def get_all_orders():
-    try:
-        conn = sqlite3.connect('internet_shop.db')
+        if not client_id.isdigit():
+            return "Ошибка: Неверный формат идентификатора клиента"
+        try:
+            conn = sqlite3.connect('i_shop.db')
+            cursor = conn.cursor()
+
+            cursor.execute('DELETE FROM clients WHERE id = ?', (client_id,))
+
+            conn.commit()
+            conn.close()
+
+            return redirect(url_for('index'))
+        except Exception as e:
+            print("Ошибка при удалении сотрудника:", e)
+            return "Ошибка при удалении сотрудника"
+
+
+def is_employee_exists(employee_id):
+    conn = sqlite3.connect('i_shop.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM employees WHERE id = ?', (employee_id,))
+    employee = cursor.fetchone()
+
+    conn.close()
+
+    return bool(employee)
+
+def is_client_exists(client_id):
+    conn = sqlite3.connect('i_shop.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM clients WHERE id = ?', (client_id,))
+    client = cursor.fetchone()
+
+    conn.close()
+
+    return bool(client)
+
+def is_product_exists(product_id):
+    conn = sqlite3.connect('i_shop.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM products WHERE id = ?', (product_id,))
+    product = cursor.fetchone()
+
+    conn.close()
+
+    return bool(product)
+@app.route('/create_order', methods=['POST'])
+def create_order():
+    if request.method == 'POST':
+        employee_id = request.form['employee_id']
+        client_id = request.form['client_id']
+        product_id = request.form['product_id']
+
+        # Разделите введенные ID товаров с помощью запятой и преобразуйте в список
+        product_id_list = [int(product_id.strip()) for product_id in product_id.split(',') if product_id.strip()]
+
+        conn = sqlite3.connect('i_shop.db')
         cursor = conn.cursor()
 
-        cursor.execute('SELECT * FROM orders')
-        orders_data = cursor.fetchall()
+        for product_id in product_id_list:
+            cursor.execute('''
+                SELECT * FROM orders
+                WHERE employee_id = ? AND client_id = ? AND product_id = ?
+            ''', (employee_id, client_id, product_id))
 
+            existing_order = cursor.fetchone()
+
+            if existing_order:
+                return f"Заказ с товаром {product_id} уже существует"
+
+            if not is_product_exists(product_id):
+                return f"Товар с ID {product_id} не существует"
+
+            cursor.execute('''
+                INSERT INTO orders (employee_id, client_id, product_id)
+                VALUES (?, ?, ?)
+            ''', (employee_id, client_id, product_id))
+
+        conn.commit()
         conn.close()
 
-        return render_template('orders_list.html', orders=orders_data)
-    except Exception as e:
-        print("Ошибка при получении списка клиентов:", e)
-        return "Ошибка при получении списка клиентов"
+        return redirect(url_for('index'))
 
+@app.route('/delete_order', methods=['POST'])
+def delete_order():
+    if request.method == 'POST':
+        order_id = request.form['order_id']
+
+        if not order_id.isdigit():
+            return "Ошибка: Неверный формат идентификатора заказа"
+        try:
+            conn = sqlite3.connect('i_shop.db')
+            cursor = conn.cursor()
+
+            cursor.execute('DELETE FROM orders WHERE id = ?', (order_id,))
+            cursor.execute('INSERT INTO shopping_cart (order_id) VALUES (?)', (order_id,))
+            conn.commit()
+            conn.close()
+
+            return redirect(url_for('index'))
+        except Exception as e:
+            print("Ошибка при удалении заказа:", e)
+            return "Ошибка при удалении заказа"
+
+
+def get_product_price_by_id(product_id):
+    conn = sqlite3.connect('i_shop.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT price FROM products WHERE id = ?', (product_id,))
+    price = cursor.fetchone()
+
+    conn.close()
+
+    return price[0] if price else None
+
+@app.route('/calculate_order_cost', methods=['POST'])
+def calculate_order_cost():
+    data = request.json
+
+    product_id = data.get('product_ids')
+
+    total_order_cost = 0
+
+    for product_id in product_id:
+        price = get_product_price_by_id(product_id)
+        if price:
+            total_order_cost += price
+
+    return jsonify({'order_cost': total_order_cost})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- createtables.py
+++ createtables.py
@@ -1,6 +1,8 @@
 import sqlite3
+import random
+import string
 
-with sqlite3.connect('internet_shop.db') as db:
+with sqlite3.connect('i_shop.db') as db:
     cursor = db.cursor()
 
 cursor.execute('''
@@ -15,6 +17,7 @@ cursor.execute('''
     CREATE TABLE IF NOT EXISTS clients (
         id INTEGER PRIMARY KEY,
         full_name TEXT NOT NULL,
+        surname TEXT NOT NULL,
         email TEXT,
         phone TEXT
     )
@@ -36,10 +39,8 @@ cursor.execute('''
         id INTEGER PRIMARY KEY,
         employee_id INTEGER,
         client_id INTEGER,
-        order_date DATE,
-        delivery_cost INTEGER,
-        order_status BOOLEAN,
         product_id INTEGER,
+        order_cost INTEGER,
         FOREIGN KEY (employee_id) REFERENCES employees (id),
         FOREIGN KEY (client_id) REFERENCES clients (id),
         FOREIGN KEY (product_id) REFERENCES products(id)
@@ -53,3 +54,46 @@ cursor.execute('''
         FOREIGN KEY (order_id) REFERENCES orders (id)
     )
 ''')
+
+# Функция для создания случайного имени продукта
+
+def generate_name():
+    return ''.join(random.choices(string.ascii_letters, k=random.randint(5, 15)))
+
+# Функция для создания случайной цены в заданном диапазоне
+def generate_price():
+    return random.randint(100, 100000)
+
+# Функция для создания случайного веса в заданном диапазоне
+def generate_weight():
+    return round(random.uniform(1, 10), 2)
+
+# Функция для создания случайного размера в заданном диапазоне
+def generate_size():
+    return round(random.uniform(0.1, 10), 2)
+
+# Функция для создания случайного текстового описания
+def generate_description():
+    return ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=random.randint(20, 100))).encode('utf-8')
+
+
+# Подключение к базе данных
+conn = sqlite3.connect('i_shop.db')
+cursor = conn.cursor()
+
+# Создание 1000 записей с валидными значениями для полей таблицы
+for _ in range(1000):
+    name = generate_name()
+    price = generate_price()
+    weight = generate_weight()
+    size = generate_size()
+    description = generate_description()
+
+    # Вставка данных в таблицу products
+    cursor.execute('''
+        INSERT INTO products (name, price, weight, size, description)
+        VALUES (?, ?, ?, ?, ?)
+    ''', (name, price, weight, size, description))
+
+conn.commit()
+conn.close()
